5fcf0665e4e0c816743f3ea571d409434767979a
chunjaemin <chun4582@naver.com>
2026-02-09 04:05:46 +0900

    [FEAT/#3] FSD 구조 도입 및 Biome, NativeWind 환경 설정

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+	return (
+		<View className="flex-1 items-center justify-center p-5">
+			<Text className="text-[32px] font-bold mb-2.5">Welcome</Text>
+			<Text>앱이 정상적으로 실행되었습니다112.</Text>
+		</View>
+	);
+}
