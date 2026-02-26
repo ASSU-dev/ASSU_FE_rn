228bfe273adedb04e4409a4f8eb25a151ec04810
KimEunHye <rladmsgp031219@naver.com>
2026-02-26 16:56:23 +0900

    [FIX/#12] Fix navigation issues(훅 규칙 위반)

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,4 +1,3 @@
-import type { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { Tabs } from "expo-router";
 import type { UserType } from "@/entities/user/model/types";
 import { BottomTabBar } from "@/widgets/bottom-tab-bar/ui/BottomTabBar";
@@ -6,21 +5,21 @@ import { BottomTabBar } from "@/widgets/bottom-tab-bar/ui/BottomTabBar";
 // TODO: 추후 useAuthStore((s) => s.userType) 로 교체
 const TEMP_USER_TYPE: UserType = "company";
 
-function TabBarAdapter({ state, navigation }: BottomTabBarProps) {
-	const activeRouteName = state.routes[state.index]?.name ?? "index";
-
-	return (
-		<BottomTabBar
-			userType={TEMP_USER_TYPE}
-			activeRouteName={activeRouteName}
-			onTabPress={(routeName) => navigation.navigate(routeName)}
-		/>
-	);
-}
-
 export default function TabLayout() {
 	return (
-		<Tabs tabBar={TabBarAdapter} screenOptions={{ headerShown: false }}>
+		<Tabs
+			screenOptions={{ headerShown: false }}
+			tabBar={({ state, navigation }) => {
+				const activeRouteName = state.routes[state.index]?.name ?? "index";
+				return (
+					<BottomTabBar
+						userType={TEMP_USER_TYPE}
+						activeRouteName={activeRouteName}
+						onTabPress={(routeName) => navigation.navigate(routeName)}
+					/>
+				);
+			}}
+		>
 			<Tabs.Screen name="index" />
 			<Tabs.Screen name="nearby" />
 			<Tabs.Screen name="coupons" />
