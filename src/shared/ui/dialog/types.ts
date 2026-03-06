9d2c06ac657e39c76b1db01021533ccba593c37c
KimEunHye <rladmsgp031219@naver.com>
2026-03-07 00:54:44 +0900

    [FEAT/#37] 추가적인 파일

---
+++ src/shared/ui/dialog/types.ts
@@ -0,0 +1,9 @@
+export interface DialogRadioContextValue {
+	value: string | null;
+	onChange: (value: string | null) => void;
+}
+
+export interface DialogSelectContextValue {
+	value: string | null;
+	onChange: (value: string | null) => void;
+}
