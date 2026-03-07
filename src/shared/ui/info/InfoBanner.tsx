438c3ae2b9153e9fe7b35589e27c93610c1c0cb1
ahcgnoej <jinjinchachajung@naver.com>
2026-03-07 22:43:42 +0900

    [STYLE/#27] 스타일 방식 통합

--- src/shared/ui/info/InfoBanner.tsx
+++ src/shared/ui/info/InfoBanner.tsx
@@ -1,5 +1,4 @@
 import { InfoFillIcon } from "@/shared/assets/icons";
-import { colorTokens } from "@/shared/styles/tokens";
 import { Text, View } from "react-native";
 
 type InfoBannerProps = {
@@ -8,15 +7,9 @@ type InfoBannerProps = {
 
 export function InfoBanner({ message }: InfoBannerProps) {
   return (
-    <View
-      className="w-full flex-row items-center rounded-[3px] px-[7px] py-[4px] gap-1"
-      style={{ backgroundColor: colorTokens.neutral }}
-    >
+    <View className="w-full flex-row items-center rounded-[3px] px-[7px] py-[4px] gap-1 bg-neutral">
       <InfoFillIcon width={10} height={10} />
-      <Text
-        className="text-[11px] flex-shrink"
-        style={{ color: colorTokens.contentSecondary }}
-      >
+      <Text className="text-[11px] flex-shrink text-content-secondary">
         {message}
       </Text>
     </View>
