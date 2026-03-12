a343ddf73497eb9e62a8d6839983d7a96da0483c
kimyw1018 <poppppp00@gmail.com>
2026-03-12 14:19:29 +0900

    Revert "[FEAT/#50] 사용자 home top랭킹 박스 구현"
    This reverts commit 6bca9b5ca304088f6a2befab661e868f463800a3.

--- src/widgets/partner-ranking/ui/PartnerRankingList.tsx
+++
@@ -1,7 +0,0 @@
-import { Text, View } from "react-native";
-
-export const PartnerRankingList = () => (
-  <View className="bg-white p-4 rounded-2xl">
-    <Text className="font-bold mb-4">Today 제휴 인기 매장</Text>
-  </View>
-);
